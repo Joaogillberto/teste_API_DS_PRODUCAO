020316c71c6660711ef43d3a72ff5d4cb717ed08
João Gilberto <107470338+Joaogillberto@users.noreply.github.com>
2023-05-06 23:51:51 -0300

    Update handler.py

--- handler.py
+++ handler.py
@@ -44,6 +44,6 @@ def rossmann_predict():
         return Reponse( '{}', status=200, mimetype='application/json' )
 
 if __name__ == '__main__':
-    os.environ.get('PORT',5000)
+   port = os.environ.get('PORT',5000)
     app.run( host = '127.0.0.1',port=port )
 
